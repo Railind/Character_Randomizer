df484e044ef6810e65c7f99096d089ceae39c3b5
railind <LindquistSocial@gmail.com>
2023-10-10 14:18:05 -0500

    Initial testing for randomizer + element adder

--- Testing.js
+++ Testing.js
@@ -2,31 +2,36 @@
 const characterDropdown = document.getElementById('characterDropdown');
 console.log(characterDropdown)
 const gridContainer = document.getElementById('gridContainer');
+let currentChars = []
 
 // Add an event listener to the dropdown to trigger when a character is selected
 characterDropdown.addEventListener('change', function () {
     const selectedCharacter = characterDropdown.value;
 
     console.log('SELECTED CHARACTER', selectedCharacter)
+    console.log(gridContainer)
 
     // Create a new div element
-    const newDiv = document.createElement('div');
-    newDiv.className = `${selectedCharacter}`;
-    newDiv.style.maxHeight = "100px"// Add a class if needed
+    if (!currentChars.includes(selectedCharacter)) {
+        const newDiv = document.createElement('div');
+        newDiv.id = `${selectedCharacter}`;
+        newDiv.style.maxHeight = "100px"// Add a class if needed
 
-    console.log('NEW DIV', newDiv.className)
+        console.log('NEW DIV', newDiv.className)
 
-    // Create a new image element
-    const newImage = document.createElement('img');
-    newImage.src = `images/${selectedCharacter}.png`;
-    newImage.style.maxHeight = "100px"
-    newImage.style.maxWidth = "100px" // Set the image source
-    console.log('IMAGE', newImage.src)
-    // Append the image to the div
-    newDiv.appendChild(newImage);
+        // Create a new image element
+        const newImage = document.createElement('img');
+        newImage.src = `images/${selectedCharacter}.png`;
+        newImage.style.maxHeight = "100px"
+        newImage.style.maxWidth = "100px" // Set the image source
+        console.log('IMAGE', newImage.src)
+        // Append the image to the div
+        newDiv.appendChild(newImage);
 
-    // Append the div to the grid container
-    gridContainer.appendChild(newDiv);
+        // Append the div to the grid container
+        gridContainer.appendChild(newDiv);
+        currentChars.push(selectedCharacter)
+    }
 });
 
 
