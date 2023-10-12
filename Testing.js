9af77e31f6a86507c1d6b43478a3fba858776473
railind <LindquistSocial@gmail.com>
2023-10-11 20:00:39 -0500

    new

--- Testing.js
+++ Testing.js
@@ -4,6 +4,9 @@ console.log(characterDropdown)
 const gridContainer = document.getElementById('gridContainer');
 let currentChars = []
 
+
+
+
 // Add an event listener to the dropdown to trigger when a character is selected
 characterDropdown.addEventListener('change', function () {
     const selectedCharacter = characterDropdown.value;
@@ -17,6 +20,8 @@ characterDropdown.addEventListener('change', function () {
         newDiv.id = `${selectedCharacter}`;
         newDiv.style.maxHeight = "100px"// Add a class if needed
 
+
+
         console.log('NEW DIV', newDiv.className)
 
         // Create a new image element
@@ -28,11 +33,27 @@ characterDropdown.addEventListener('change', function () {
         // Append the image to the div
         newDiv.appendChild(newImage);
 
+        //Creating our remove button
         const remove = document.createElement('button')
         remove.innerText = "X"
         remove.style.background = "none"
         remove.style.borderRadius = "5px"
         remove.style.border = "grey 1px solid"
+        remove.style.opacity = "0%"
+        //Event listener that does the removing
+        remove.addEventListener("click", () => {
+            newDiv.remove()
+            currentChars.splice(currentChars.indexOf(selectedCharacter), 1)
+        })
+        //Listeners for the sake of style
+        newDiv.addEventListener("mouseenter", () => {
+            remove.style.opacity = "100%"
+            newDiv.style.boxShadow = "2px 2px black"
+        })
+        newDiv.addEventListener("mouseleave", () => {
+            remove.style.opacity = "0%"
+            newDiv.style.boxShadow = ""
+        })
 
         newDiv.appendChild(remove)
 
@@ -41,7 +62,50 @@ characterDropdown.addEventListener('change', function () {
         currentChars.push(selectedCharacter)
         // localStorage.setItem()
     }
+
+
+
+
+
+
 });
+
+function LegendRandomizer() {
+    return currentChars[Math.floor(Math.random() * currentChars.length)]
+}
+
+function LegendPicker() {
+    let message;
+    let chosen = LegendRandomizer()
+    console.log(chosen)
+
+    let imagePath = `images/${chosen.toLowerCase()}.png`; // Construct the image path
+    let legendImage = `<img src="${imagePath}" alt="${chosen}">`; // Create an img element with the legend image
+
+
+    if (chosen === 'Pathfinder') {
+        message = 'Pathy Message'
+    }
+    if (chosen === 'Valkyrie') {
+        message = 'Valk Message'
+    }
+    if (chosen === 'Wattson') {
+        message = "Watty Message"
+    }
+    if (chosen === 'Ballistic') {
+        message = "Bally Message"
+    }
+    if (chosen === 'Ashe') {
+        message = "Ashy Message"
+    }
+    if (chosen === 'Vantage') {
+        message = "Vanty Message"
+    }
+    console.log(message);
+
+    document.getElementById("legendMessage").innerHTML = message;
+    document.getElementById("legendImage").innerHTML = legendImage;
+}
 
 
 //IT WORKS
