9c9066dcc155261adf9761dbc20a285aeb8fbd5d
railind <LindquistSocial@gmail.com>
2023-10-24 17:32:36 -0500

    All legends added

---
+++ Legends.js
@@ -0,0 +1,4 @@
+{
+    name: "ash",
+        message: "hello"
+} ''
--- Testing.js
+++ Testing.js
@@ -18,7 +18,9 @@ characterDropdown.addEventListener('change', function () {
     if (!currentChars.includes(selectedCharacter)) {
         const newDiv = document.createElement('div');
         newDiv.id = `${selectedCharacter}`;
-        newDiv.style.maxHeight = "100px"// Add a class if needed
+        newDiv.style.maxHeight = "150px"
+        newDiv.style.margin = "10px"// Add a class if needed
+        newDiv.style.borderRadius = "5px"
 
 
 
@@ -27,19 +29,25 @@ characterDropdown.addEventListener('change', function () {
         // Create a new image element
         const newImage = document.createElement('img');
         newImage.src = `images/${selectedCharacter}.png`;
-        newImage.style.maxHeight = "100px"
-        newImage.style.maxWidth = "100px" // Set the image source
+        newImage.style.maxHeight = "150px"
+        newImage.style.maxWidth = "150px" // Set the image source
         console.log('IMAGE', newImage.src)
         // Append the image to the div
         newDiv.appendChild(newImage);
 
         //Creating our remove button
         const remove = document.createElement('button')
-        remove.innerText = "X"
+        remove.style.display = "flex"
+        remove.style.color = "white"
         remove.style.background = "none"
         remove.style.borderRadius = "5px"
         remove.style.border = "grey 1px solid"
         remove.style.opacity = "0%"
+        remove.style.backgroundColor = "grey"
+        // remove.style.margins = "10px"
+        // remove.style.padding = "20px"
+        remove.style.position = "relative"
+        remove.innerText = "X"
         //Event listener that does the removing
         remove.addEventListener("click", () => {
             newDiv.remove()
@@ -48,11 +56,13 @@ characterDropdown.addEventListener('change', function () {
         //Listeners for the sake of style
         newDiv.addEventListener("mouseenter", () => {
             remove.style.opacity = "100%"
-            newDiv.style.boxShadow = "2px 2px black"
+            // newDiv.style.boxShadow = "2px 2px black"
+            newDiv.style.border = "black 1px solid"
         })
         newDiv.addEventListener("mouseleave", () => {
             remove.style.opacity = "0%"
             newDiv.style.boxShadow = ""
+            newDiv.style.border = ""
         })
 
         newDiv.appendChild(remove)
@@ -83,25 +93,25 @@ function LegendPicker() {
     let legendImage = `<img src="${imagePath}" alt="${chosen}">`; // Create an img element with the legend image
 
 
-    if (chosen === 'Pathfinder') {
+    if (chosen === 'pathfinder') {
         message = 'Pathy Message'
     }
-    if (chosen === 'Valkyrie') {
+    if (chosen === 'valkyrie') {
         message = 'Valk Message'
     }
-    if (chosen === 'Wattson') {
+    if (chosen === 'wattson') {
         message = "Watty Message"
     }
-    if (chosen === 'Ballistic') {
+    if (chosen === 'ballistic') {
         message = "Bally Message"
     }
-    if (chosen === 'Ashe') {
+    if (chosen === 'ashe') {
         message = "Ashy Message"
     }
-    if (chosen === 'Vantage') {
+    if (chosen === 'vantage') {
         message = "Vanty Message"
     }
-    console.log(message);
+    console.log(message, "MESSAGE");
 
     document.getElementById("legendMessage").innerHTML = message;
     document.getElementById("legendImage").innerHTML = legendImage;
